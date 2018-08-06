0a493a8b9d2b98f50c7f55d1a602cbc5dbe3f908
Muhammad Aleem Tahir <aleem.tahir@gmail.com>
2018-08-06 22:40:51 +0500

    response temp

--- resources/views/forms/response.blade.php
+++ resources/views/forms/response.blade.php
@@ -21,11 +21,11 @@
 					<br>
 					<div id="response-input" class="row">
 						<div class="col-md-12">
-							<div id="msg"><span >Select the valid Id to merge record in the following documents.</span></div>
+							<div id="msg"><span >*Select the valid Id to merge data in the following documents.</span></div>
 						</div>
 						<div class="col-md-12">
 		                    <div style=" width: 265px;" class="c-field c-text-singleline c-col-17 c-sml-col-1 c-span-8 c-sml-span-2">
-		                    	<div  style="padding-left: 8px; "><strong>Record ID</strong></div>
+		                    	<div  style="padding-left: 8px; "><strong>Volume Folio Lot No.</strong></div>
 			                    <div class="c-editor">
 			                    	<input name="autocomplete" type="text" id="autocomplete" autocomplete="off" placeholder="Volume Folio Lot">
 			                    {{-- <div class="c-helptext">SEARCH RECORD ID.</div> --}}
