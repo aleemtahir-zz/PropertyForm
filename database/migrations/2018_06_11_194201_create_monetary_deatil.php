5d19e3808719d10f00f53a4895f2798969faeb01
Muhammad Aleem Tahir <aleem.tahir@gmail.com>
2018-09-27 21:29:45 +0500

    make currency fields string

--- database/migrations/2018_06_11_194201_create_monetary_deatil.php
+++ database/migrations/2018_06_11_194201_create_monetary_deatil.php
@@ -17,21 +17,21 @@ class CreateMonetaryDeatil extends Migration
             $table->increments('id');
             $table->integer('fc_id')->unsigned()->nullable(); 
             $table->foreign('fc_id')->references('id')->on('tbl_foriegn_currency'); 
-            $table->integer('price_i')->nullable();
+            $table->string('price_i')->nullable();
             $table->string('price_w')->nullable();
-            $table->integer('j_price_i')->nullable();
+            $table->string('j_price_i')->nullable();
             $table->string('j_price_w')->nullable();
-            $table->integer('deposit')->nullable();
-            $table->integer('second_payment')->nullable();
-            $table->integer('final_payment')->nullable();
-            $table->float('half_title')->nullable();
-            $table->float('half_land_agreement')->nullable();
-            $table->float('half_build_agreement')->nullable();
-            $table->float('half_stamp_duty')->nullable();
-            $table->float('half_reg_fee')->nullable();
-            $table->integer('inc_cost')->nullable();
-            $table->integer('maintenance_expense')->nullable();
-            $table->integer('identification_fee')->nullable();
+            $table->string('deposit')->nullable();
+            $table->string('second_payment')->nullable();
+            $table->string('final_payment')->nullable();
+            $table->string('half_title')->nullable();
+            $table->string('half_land_agreement')->nullable();
+            $table->string('half_build_agreement')->nullable();
+            $table->string('half_stamp_duty')->nullable();
+            $table->string('half_reg_fee')->nullable();
+            $table->string('inc_cost')->nullable();
+            $table->string('maintenance_expense')->nullable();
+            $table->string('identification_fee')->nullable();
         });
     }
 
